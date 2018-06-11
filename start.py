68d15693eb9655351f648a66e2dff64e6e2ca694
Tz21 <timjun21@gmail.com>
2018-06-11 22:14:36 +0800

    initial commit

---
+++ start.py
@@ -0,0 +1,31 @@
+#數字
+print(21)
+#字串
+print("python字串")
+#布林值
+print(True)
+#變數
+input = 3
+print(input)
+#運算符號
+data = 3 + 5
+print(data)
+data = 5 / 2
+print(data)
+data = 5 // 2 #整數除法
+print(data)
+data = 6 * 2
+print(data)
+data = 6**2 #二次方
+print(data)
+print('Hello'+'World')
+#提示文字
+n1 = input("Enter a number:")
+n2 = input("Enter another number:")
+result = n1 + n2
+print(result)
+n1 = input("Enter a number:")
+n2 = input("Enter another number:")
+result2 = int(n1) + int(n2)
+print(result2)
+#homework:讓使用者輸入5個數字，1.算總和 2.哪個數字最大
